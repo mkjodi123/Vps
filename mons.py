a2650e95e968460b2ade726163f167bd42142640
mkjodi123 <m.ohit8684944463@gmail.com>
2025-02-10 21:00:40 +0530

    Add files via upload

--- mons.py
+++ mons.py
@@ -9,7 +9,7 @@ attack_in_progress = False
 current_attack = None  # Store details of the current attack
 attack_history = []  # Store attack logs
 
-TELEGRAM_BOT_TOKEN = '7591376778:AAFxaoD6j9s_7iejKBGm7lbwH-XQlsZd1_E'
+TELEGRAM_BOT_TOKEN = '7591376778:AAF0eOTmZsutUAmdgGldDbt-q5tJoqRn7yE'
 ADMIN_USER_ID = 7353797869
 MONGO_URI = "mongodb+srv://Kamisama:Kamisama@kamisama.m6kon.mongodb.net/"
 DB_NAME = "Roxz"
