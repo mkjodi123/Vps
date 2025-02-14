05ee538fb82398a51f40906493364ca91aee75da
mkjodi123 <m.ohit8684944463@gmail.com>
2025-02-14 13:26:35 +0530

    Add files via upload

--- daku.py
+++ daku.py
@@ -9,7 +9,7 @@ attack_in_progress = False
 current_attack = None  # Store details of the current attack
 attack_history = []  # Store attack logs
 
-TELEGRAM_BOT_TOKEN = '7591376778:AAEV6h7CikULkfJlL6DLxKDxQ35SaQfTmUE'
+TELEGRAM_BOT_TOKEN = '7591376778:AAGdPlDzEHhxR3z9hwYtZpuYW8DWtgiFhZA'
 ADMIN_USER_ID = 7353797869
 MONGO_URI = "mongodb+srv://Kamisama:Kamisama@kamisama.m6kon.mongodb.net/"
 DB_NAME = "Roxz"
